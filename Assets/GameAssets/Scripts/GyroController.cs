61cb03b4cfacfae565dee8be85ba79034223a128
William <wah20@kent.ac.uk>
2023-11-06 12:00:33 +0000

    Adding the compass game to it

--- Assets/GameAssets/Scripts/GyroController.cs
+++ Assets/GameAssets/Scripts/GyroController.cs
@@ -5,6 +5,7 @@ public class GyroController : MonoBehaviour
     private bool metGoal = false;
     private Gyroscope gyro;
     public GameObject GoalPos;
+    public GameObject background;
 
     private void Start()
     {
@@ -37,6 +38,18 @@ public class GyroController : MonoBehaviour
             targetEulerAngles.x = 0.0f; // set z to 0 to ensure it only rotates around the y-axis
 
             transform.eulerAngles = targetEulerAngles;
+            Vector3 rotation = transform.rotation.eulerAngles;
+            float rotatedZ = rotation.z > 220 ? rotation.z - 360 : rotation.z;
+            // Map rotation to position
+            float t = Mathf.InverseLerp(-200, 220, rotatedZ);
+            Debug.Log(rotatedZ);
+            Debug.Log(t);
+            float targetPosX = Mathf.Lerp(-4.2f, 4.5f, t);
+
+            // Move the background
+            Vector3 backgroundPosition = background.transform.position;
+            backgroundPosition.x = targetPosX;
+            background.transform.position = backgroundPosition;
         }
     }
 }
