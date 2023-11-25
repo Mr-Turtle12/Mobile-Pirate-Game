56e938cfd5f380226d7e2a0ca989c496740c87aa
William <wah20@kent.ac.uk>
2023-11-25 10:14:46 +0000

    Add the startMiniGameScreen

--- Assets/GameAssets/Scripts/CompassGame/CompassController.cs
+++ Assets/GameAssets/Scripts/CompassGame/CompassController.cs
@@ -11,6 +11,8 @@ public class CompassController : MonoBehaviour
     public GameObject background;
     private int Count;
     private GyroController phoneController;
+    public GameObject newCompassAnimationprefab;
+    public StartMiniGame Starter;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,29 +25,34 @@ public class CompassController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 previousEulerAngles = Compass_hand.eulerAngles;
-
-        if (Mathf.Abs(previousEulerAngles.z - Goal_hand.rotation.eulerAngles.z) <= 2)
+        if (Starter.start)
         {
-            Goal_hand.GetComponent<RandomZPos>().ChangePos();
-            Count++;
+            Vector3 previousEulerAngles = Compass_hand.eulerAngles;
+
+            if (Mathf.Abs(previousEulerAngles.z - Goal_hand.rotation.eulerAngles.z) <= 2)
+            {
+                GameObject Animation = Instantiate(newCompassAnimationprefab) as GameObject;
+                Destroy(Animation, 0.25f);
+                Goal_hand.GetComponent<RandomZPos>().ChangePos();
+                Count++;
+            }
+            if (Count <= 3)
+            {
+                Compass_hand.eulerAngles = phoneController.AddGyro(previousEulerAngles, false, false, true);
+
+                Vector3 rotation = Compass_hand.rotation.eulerAngles;
+                float rotatedZ = rotation.z > 220 ? rotation.z - 360 : rotation.z;
+
+                // Map rotation to position
+                float t = Mathf.InverseLerp(-200, 220, rotatedZ);
+                float targetPosX = Mathf.Lerp(-4.2f, 4.5f, t);
+
+                // Move the background
+                Vector3 backgroundPosition = background.transform.position;
+                backgroundPosition.x = targetPosX;
+                background.transform.position = backgroundPosition;
+            }
+
         }
-        if (Count <= 3)
-        {
-            Compass_hand.eulerAngles = phoneController.AddGyro(previousEulerAngles, false, false, true);
-
-            Vector3 rotation = Compass_hand.rotation.eulerAngles;
-            float rotatedZ = rotation.z > 220 ? rotation.z - 360 : rotation.z;
-
-            // Map rotation to position
-            float t = Mathf.InverseLerp(-200, 220, rotatedZ);
-            float targetPosX = Mathf.Lerp(-4.2f, 4.5f, t);
-
-            // Move the background
-            Vector3 backgroundPosition = background.transform.position;
-            backgroundPosition.x = targetPosX;
-            background.transform.position = backgroundPosition;
-        }
-
     }
 }
