3d06aca7c5036031d8f45fa0e140bbb8e28bc64c
William <wah20@kent.ac.uk>
2023-11-26 13:09:02 +0000

    Add it so you can select  different gamemode

--- Assets/GameAssets/Scripts/MainMenu/AddName.cs
+++ Assets/GameAssets/Scripts/MainMenu/AddName.cs
@@ -11,17 +11,20 @@ using UnityEngine.UI;
 public class AddName : MonoBehaviour
 {
     public Button plus;
+    public Button Play;
+    public DataCarrierScript dataCarrier;
     public GameObject[] players;
-    public TextMeshProUGUI PlayerNameTextField;
-    public List<string> PlayersName;
+    public TextMeshProUGUI playerNameTextField;
+    public List<string> playersName;
     // Start is called before the first frame update
     void Start()
     {
         plus.onClick.AddListener(AddPlayer);
-        PlayersName = new List<string>();
-        foreach (GameObject player in players)
+        Play.onClick.AddListener(playGame);
+        playersName = new List<string>();
+        foreach (GameObject objPlayers in players)
         {
-            player.SetActive(false);
+            objPlayers.SetActive(false);
         }
 
     }
@@ -31,16 +34,36 @@ public class AddName : MonoBehaviour
     {
 
     }
+    void playGame()
+    {
+        dataCarrier.SetPlayerList(playersName);
+        GameMode gameMode = dataCarrier.GetGameMode();
+        if (gameMode.Equals(GameMode.FreePlay))
+        {
+            //Load Main map
+            Debug.Log("Load up main map");
+        }
+        else if (gameMode.Equals(GameMode.Endless))
+        {
+            //Load up a random mini game
+            Debug.Log("Load up a random miniGame");
+        }
+        else if (gameMode.Equals(GameMode.Vs))
+        {
+            //Load first game
+            Debug.Log("Load first miniGame");
+        }
+    }
     void AddPlayer()
     {
-        int CurrentPlayerCount = PlayersName.Count;
+        int CurrentPlayerCount = playersName.Count;
         if (CurrentPlayerCount <= 7)
         {
 
             players[CurrentPlayerCount].SetActive(true);
-            string name = PlayerNameTextField.text;
+            string name = playerNameTextField.text;
             players[CurrentPlayerCount].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = name;
-            PlayersName.Add(name);
+            playersName.Add(name);
 
         }
 
