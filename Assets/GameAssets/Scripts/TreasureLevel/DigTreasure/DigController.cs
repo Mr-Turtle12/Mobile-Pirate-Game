9124cd8ba04669369bbd6e746c2b998ca5de2c11
William <wah20@kent.ac.uk>
2023-11-27 20:49:32 +0000

    Added new dig

---
+++ Assets/GameAssets/Scripts/TreasureLevel/DigTreasure/DigController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigController : MonoBehaviour
+{
+    public float fallThreshold = 0.5f;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public GameObject spot;
+    public float gameTime = 5f;
+    public float coolDownTime = 0.5f;
+
+    private ThrustController thrushController;
+    private CountdownController countdown;
+    private bool IsGame;
+    private float total = 0;
+
+    void Start()
+    {
+        thrushController = gameObject.AddComponent<ThrustController>();
+        countdown = gameObject.GetComponent<CountdownController>();
+        IsGame = true;
+        thrushController.setCoolDownTime(coolDownTime);
+        animator.speed = coolDownTime;
+        StartCoroutine(Timer());
+    }
+
+    void Update()
+    {
+        if (countdown.start)
+        {
+            float thrushValue = thrushController.GetThrush(fallThreshold);
+            if (thrushValue > 0)
+            {
+                spot.SetActive(false);
+                total = total + thrushValue;
+                animator.SetTrigger("Move");
+                Debug.Log(total);
+
+
+                Color newColor = spriteRenderer.color;
+                newColor.a = total / 10;
+                spriteRenderer.color = newColor;
+            }
+        }
+    }
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(gameTime);
+        Color newColor = spriteRenderer.color;
+        Debug.Log(newColor);
+        Debug.Log("Coin found:" + newColor.a * 200);
+        countdown.endGame();
+    }
+
+}
+
